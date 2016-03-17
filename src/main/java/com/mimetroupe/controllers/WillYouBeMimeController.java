5de2fb963df1dee4701b48ec55aaac3d5dfce87c
BrandenSandahl <branden157@gmail.com>
2016-03-17 16:49:17 -0400

    changed stuff

--- src/main/java/com/mimetroupe/controllers/WillYouBeMimeController.java
+++ src/main/java/com/mimetroupe/controllers/WillYouBeMimeController.java
@@ -45,7 +45,9 @@ public class WillYouBeMimeController {
     @RequestMapping(path = "/mime", method = RequestMethod.POST)
     public Mime createMime(@RequestBody Mime mime) throws Exception {
 
-        if (mimeRepository.findByUserName(mime.getUserName()) != null) {
+        Mime mimeDb = mimeRepository.findByUserName(mime.getUserName());
+
+        if (mimeDb == null) {
             mime.setPassword(PasswordStorage.createHash(mime.getPassword()));
             return mimeRepository.save(mime); //RUN A TEST TO SEE IF THIS HASHED
         } else {
